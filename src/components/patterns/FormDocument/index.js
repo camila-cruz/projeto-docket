694aca6eb55dfcef85a5d3ac16405caaf9b26746
Camila <cruz.camila001@gmail.com>
2021-06-13 16:02:21 -0300

    refactor: form document spacing

--- src/components/patterns/FormDocument/index.js
+++ src/components/patterns/FormDocument/index.js
@@ -4,48 +4,79 @@ import Box from '../../foundation/Box';
 import Text from '../../foundation/Text';
 import './index.scss';
 
+function FormGroup({ children, obrigatory }) {
+  return (
+    <div className={`FormGroup ${obrigatory ? 'obrigatory' : '' }`}>
+      {children}
+    </div>
+  )
+}
+
+function FormSection({ children }) {
+  return (
+    <div className="FormSection">
+      {children}
+    </div>
+  )
+}
+
 export default function FormDocument() {
   return (
     <Box className="FormDocument">
       <Text tag="h4">
         Adicionar documentos ao pedido
       </Text>
-      <Text tag="label">Nome do documento: *</Text>
-      <TextField type="text" placeholder="Digite aqui" />
-      <Text tag="label">Tipo de pessoa: *</Text>
-      <TextField type="text" placeholder="Digite aqui" />
-      <Text tag="label">CPF *</Text>
-      <TextField type="text" placeholder="Digite aqui" />
-      <Text tag="label">Nome completo: *</Text>
-      <TextField type="text" placeholder="Digite aqui" />
-      
-      <Text tag="h6">Dados do cartório</Text>
 
-      <Text tag="label">CEP: *</Text>
-      <TextField type="text" placeholder="Digite aqui" />
+      <form>
+        <FormGroup obrigatory>
+          <Text tag="label">Nome do documento: </Text>
+          <TextField type="text" placeholder="Digite aqui" />
+        </FormGroup>
 
-      <div>
-        <div>
-          <Text tag="label">Rua: *</Text>
+        <FormGroup obrigatory>
+          <Text tag="label">Tipo de pessoa: </Text>
           <TextField type="text" placeholder="Digite aqui" />
-        </div>
-        <div>
-          <Text tag="label">Número: *</Text>
-          <TextField type="text" placeholder="Digite aqui" />
-        </div>
-      </div>
+        </FormGroup>
 
-      <div>
-        <div>
-          <Text tag="label">Cidade: *</Text>
+        <FormGroup obrigatory>
+          <Text tag="label">CPF </Text>
           <TextField type="text" placeholder="Digite aqui" />
-        </div>
-        <div>
-          <Text tag="label">UF: *</Text>
+        </FormGroup>
+
+        <FormGroup obrigatory>
+          <Text tag="label">Nome completo: </Text>
           <TextField type="text" placeholder="Digite aqui" />
-        </div>
-      </div>
-      
+        </FormGroup>
+        
+        <Text tag="h4" className="subtitle">Dados do cartório</Text>
+
+        <FormGroup obrigatory>
+          <Text tag="label">CEP: </Text>
+          <TextField type="text" placeholder="Digite aqui" />
+        </FormGroup>
+
+        <FormSection>
+          <FormGroup obrigatory>
+            <Text tag="label">Rua: </Text>
+            <TextField type="text" placeholder="Digite aqui" />
+          </FormGroup>
+          <FormGroup obrigatory>
+            <Text tag="label">Número: </Text>
+            <TextField type="text" placeholder="Digite aqui" />
+          </FormGroup>
+        </FormSection>
+
+        <FormSection>
+          <FormGroup obrigatory>
+            <Text tag="label">Cidade: </Text>
+            <TextField type="text" placeholder="Digite aqui" />
+          </FormGroup>
+          <FormGroup obrigatory>
+            <Text tag="label">UF: </Text>
+            <TextField type="text" placeholder="Digite aqui" />
+          </FormGroup>
+        </FormSection>
+      </form>
     </Box>
   )
 }
